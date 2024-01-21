fbab3c0854916a1820722f48372a0a0f852e931b
william <william.peralta18@gmail.com>
2024-01-21 15:36:44 +0100

    EDX-3 fix deps

--- resources/views/backoffice-app.blade.php
+++ resources/views/backoffice-app.blade.php
@@ -9,16 +9,14 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+        <link href="/assets/css/jquery-ui.min.css" rel="stylesheet" />
+        <script src="/assets/js/jquery-3.7.1.min.js"></script>
+        <script src="/assets/js/jquery-ui.min.js"></script>
         <!-- Scripts -->
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
         @inertiaHead
-        <link href="/assets/css/jquery-ui.min.css" rel="stylesheet" />
-
-        <script src="/assets/js/jquery-3.7.1.min.js"></script>
-        <script src="/assets/js/jquery-ui.min.js"></script>
     </head>
     <body class="font-sans antialiased" id="root">
         @inertia('backoffice-app')
