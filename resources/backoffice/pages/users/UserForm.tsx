f9fb38b55682f2cba5c3c7130b6324673e94557d
william <william.peralta18@gmail.com>
2024-01-29 11:33:57 +0100

    aggiunti campi

--- resources/backoffice/pages/users/UserForm.tsx
+++ resources/backoffice/pages/users/UserForm.tsx
@@ -10,6 +10,10 @@ const UserForm = (props: any) => {
             <Field component={InputText} name="first_name" label="Nome" />
             <Field component={InputText} name="last_name" label="Cognome" />
             <Field component={InputText} name="email" label="Email" />
+            <Field component={InputText} name="phone" label="Telefono" />
+            <Field component={InputText} name="country" label="Paese" />
+            <Field component={InputText} name="country_code" label="CAP" />
+            <Field component={InputText} name="role" label="Ruolo" />
             <Field component={Switch} name="active" label="Attivo" />
         </div>
     )
@@ -17,6 +21,6 @@ const UserForm = (props: any) => {
 
 export default withModalForm({
     apiResource: "/api/users",
-    title: "Utente",
+    title: "Modifica Utente",
     width: 600
 })(UserForm)
