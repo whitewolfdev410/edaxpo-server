baba29f8243ef25126171c559f0a9208043323da
william <william.peralta18@gmail.com>
2024-01-29 11:33:39 +0100

    font-family

--- resources/views/backoffice-app.blade.php
+++ resources/views/backoffice-app.blade.php
@@ -10,6 +10,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
         <link href="/assets/css/jquery-ui.min.css" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap" rel="stylesheet">
         <script src="/assets/js/jquery-3.7.1.min.js"></script>
         <script src="/assets/js/jquery-ui.min.js"></script>
         <!-- Scripts -->
